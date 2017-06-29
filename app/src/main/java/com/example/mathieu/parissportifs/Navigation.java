04c7b5fbc1a17b41dcf6beed8cd99960cdd8c46b
Zilio Bruno <bruno.zilio31@gmail.com>
2017-06-29 16:35:16 +0200

    intent imageview/xml

--- app/src/main/java/com/example/mathieu/parissportifs/Navigation.java
+++ app/src/main/java/com/example/mathieu/parissportifs/Navigation.java
@@ -6,6 +6,8 @@ import android.support.annotation.IdRes;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.roughike.bottombar.BottomBar;
@@ -15,6 +17,7 @@ public class Navigation extends AppCompatActivity {
 
     private BottomBar bottomBar;
     private String key;
+    private ImageView imageViewSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +28,7 @@ public class Navigation extends AppCompatActivity {
         Bundle bundle = intent.getBundleExtra(CreateOrJoinCompetition.COMPETITION_ID);
         key = bundle.getString(CreateOrJoinCompetition.COMPETITION_ID);
 
+        imageViewSettings = (ImageView) findViewById(R.id.imageViewSettings);
 
         bottomBar = (BottomBar) findViewById(R.id.bottomBar);
 
@@ -49,7 +53,17 @@ public class Navigation extends AppCompatActivity {
                 transaction.commit();
             }
         });
+        imageViewSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Navigation.this, ModifyProfile.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
     }
+
 
     public String getKey() {
         return key;
